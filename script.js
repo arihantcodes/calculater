d5a5c34f547fed9e70d3b99f82671dcfcd015349
Arihant jain <138019782+arihant-jain0@users.noreply.github.com>
2023-11-25 15:44:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let string = "";
+let button = document.querySelectorAll(".button");
+Array.from(button).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      document.querySelector("input").value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      document.querySelector("input").value = string;
+    } else if (e.target.innerHTML == "X") {
+      string += "*";
+      document.querySelector("input").value = string;
+    } else if (e.target.innerHTML == "÷") {
+      string += "/";
+      document.querySelector("input").value = string;
+    } else if (e.target.innerHTML == "C") {
+      string = string.slice(0, -1);
+      document.querySelector("input").value = string;
+    }
+    
+    else {
+      console.log(e.target);
+      string = string + e.target.innerHTML;
+      document.querySelector("input").value = string;
+    }
+  });
+});
